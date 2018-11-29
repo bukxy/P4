21a2e973c2e51f6627f12b0b1285b537f5b26f61
bukxy <peron.nicolas2@gmail.com>
2018-11-29 12:00:35 +0100

    Ajout de class css et d'un format de date

--- app/views/front/postView.php
+++ app/views/front/postView.php
@@ -1,7 +1,8 @@
-<?php $title = 'Mon Blog'; ?>
+<?php $title = htmlspecialchars($post->getTitle()); ?>
 
 <?php ob_start(); ?>
-    <h1 class="h1">Post view</h1>
+
+    <h1 class="h1"><?= htmlspecialchars($post->getTitle()); ?> </h1>
         
     <div class="lignesoush1">
         <div class="rondh1bleu left"></div>
@@ -10,22 +11,22 @@
         <div class="rondh1bleu topLeft"></div>
     </div>
 
-    <p><a href="index.php?p=blog">Retour à la liste des articles</a></p>
+    <p class="backToBlog"><a href="index.php?p=blog">Retour à la liste des articles</a></p>
 
     <section>
 
         <div class="onePostContainer">
-            <h3> 
-                <?= htmlspecialchars($post->getTitle()); ?> 
-            </h3>
 
             <p> 
-                <em>le <?= $post->getDate();?></em> par <?= $post->getAuthor(); ?> 
+                <em>le <?= date('d/m/Y à H:i', strtotime($post->getDate()))?></em> par <?= $post->getAuthor(); ?> 
             </p>
+
+            <div class="barre"></div>
             
             <p class="myPost"> 
                 <?= $post->getPost()?> 
             </p>
+
         </div>
 
     </section>
@@ -38,11 +39,11 @@
 
                 <div class=postPseudo>
                     <label for="author">Votre pseudo :</label><br />
-                    <input type="text" id="author" name="author" maxlength="12" required />
+                    <input type="text" id="author" name="author" maxlength="15" required />
                 </div>
                 <div class="postMessage">
                     <label for="comment">Votre message :</label><br />
-                    <textarea id="comment" name="comment" required ></textarea>
+                    <textarea id="comment" name="comment" maxlength="255" required ></textarea>
                 </div>
 
                 <input type="submit" value="Poster le commentaire" />
@@ -54,33 +55,40 @@
         <div>
 
             <div>   
-                <h3>Commentaires</h3>
+                <h3>Vos commentaires</h3>
             </div>
 
-            <?php foreach ($comments as $comment): ?>
+            <?php if ($comments) {
 
-            <div class="myCommentContainer">
+                foreach ($comments as $comment): ?>
 
-                <p class="commentInfo">
-                    De <strong><?= htmlspecialchars(strip_tags($comment->getAuthor())); ?></strong> le <em><?= $comment->getDate(); ?></em> | 
+                <div class="myCommentContainer">
 
-                    <?php if (!isset($_COOKIE['idCommentReport-'. $comment->getId()])) {
+                    <p class="commentInfo">
+                        De <strong><?= htmlspecialchars(strip_tags($comment->getAuthor())); ?></strong> le <em><?= date('d/m/Y à H:i', strtotime($comment->getDate()))?></em> | 
 
-                        echo $_COOKIE['idCommentReport-'. $comment->getId()]; ?>
-                        <a href="index.php?p=reportComment&amp;id=<?= $comment->getId() ?>"><button class="buttonReport"><i class="fas fa-comment-slash"></i></button></a>
+                        <?php if (!isset($_COOKIE['idCommentReport-'. $comment->getId()])) {
 
-                    <?php } else { ?>
-                        Signalé
-                    <?php } ?>
-                </p>
+                            echo $_COOKIE['idCommentReport-'. $comment->getId()]; ?>
+                            <a href="index.php?p=reportComment&amp;id=<?= $comment->getId() ?>"><button class="buttonReport"><i class="fas fa-comment-slash"></i></button></a>
 
-                <p class="theComment">
-                    <?= htmlspecialchars(strip_tags($comment->getComment())); ?>
-                </p>
+                        <?php } else { ?>
+                            Signalé
+                        <?php } ?>
+                    </p>
 
-            </div>
+                    <p class="theComment">
+                        <?= htmlspecialchars(strip_tags($comment->getComment())); ?>
+                    </p>
 
+                </div>
+            
             <?php endforeach; ?>
+
+            <?php } else { ?>
+                <p>Il n'y a pour le moment aucun commentaire</p>
+            <?php } ?>
+
         </div>
     </section>
 
