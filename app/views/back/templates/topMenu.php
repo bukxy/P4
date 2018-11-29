3c8269014e113dda6456aeafc54d7a937b704e22
bukxy <peron.nicolas2@gmail.com>
2018-11-29 11:59:18 +0100

    Suppresion de balises bootstrap et ajout de class css pour adaptation responsive + menu sandwich

--- app/views/back/templates/topMenu.php
+++ app/views/back/templates/topMenu.php
@@ -1,5 +1,10 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php?p=blog">Jean Forteroche</a>
+
+    <div class="mobileNavMenuBack">
+      <i class="fas fa-bars fa-2x"></i>
+    </div>
+
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="index.php?p=disconnect">Déconnection</a>
