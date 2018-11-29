3c14af4ec221559aacac574adc18b577e044a46f
bukxy <peron.nicolas2@gmail.com>
2018-11-29 11:58:32 +0100

    Suppresion de balises bootstrap et ajout de class css pour adaptation responsive

--- app/views/back/templates/leftMenu.php
+++ app/views/back/templates/leftMenu.php
@@ -1,16 +1,22 @@
 <nav class="col-3 col-md-2 d-md-block bg-light sidebar" id="">
-  <div class="sidebar-sticky">
+  <div class="myBackNavbar">
     <ul class="nav flex-column">
       <li class="nav-item">
         <a class="nav-link" href="index.php?p=admin">
           <i class="fas fa-file-alt"></i>
-          Gestion des Articles
+          Articles
         </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="index.php?p=comments">
           <i class="fas fa-comment-dots"></i>
-          Gestion des Commentaires
+          Commentaires
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?p=usersList">
+          <i class="fas fa-user-cog"></i>
+          Utilisateurs
         </a>
       </li>
     </ul>
