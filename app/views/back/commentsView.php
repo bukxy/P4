38734017f2741928f0ab8a9a696309581b026b61
bukxy <peron.nicolas2@gmail.com>
2018-11-29 11:51:35 +0100

    Ajout d'un tableau pour les informations des commentaires

--- app/views/back/commentsView.php
+++ app/views/back/commentsView.php
@@ -2,36 +2,65 @@
 session_start();
 ob_start(); 
 
-if (isset($_SESSION['pseudo'])) { ?>
+if (isset($_SESSION['pseudo'])) {
+
+    if (isset($_SESSION['notificationAdminYes'])) {?>
+        
+        <div class="notifAdmin notifAdminGreen">
+            <p>
+                <?= $_SESSION['notificationAdminYes'];
+                unset($_SESSION['notificationAdminYes']); ?>
+            </p>
+        </div>
+
+    <?php } elseif (isset($_SESSION['notificationAdminNo'])) {?>
+        
+        <div class="notifAdmin notifAdminRed">
+            <p>
+                <?= $_SESSION['notificationAdminNo'];
+                unset($_SESSION['notificationAdminNo']); ?>
+            </p>
+        </div>
+
+    <?php } ?>
 
     <?php $title = 'Gestion des commentaires'; ?>
     <?php $titleCat = 'Gestion des commentaires'; ?>
 
-    <table>
+    <section class="commentList">
 
-        <tr>
-            <td>Auteur</td>
-            <td>Commentaire</td>
-            <td>Date</td>
-            <td>Signalements</td>
-        </tr>
- 
-        <?php foreach ($allComments as $comments): ?>
+        <table>
 
-        <tr>
-            <td><?= $comments->getAuthor();?></td>
-            <td><?= substr(strip_tags($comments->getComment()), 0, 60)?></td>
-            <td><?= $comments->getDate();?></td>
-            <td><?= $comments->getReport();?></td>
-            <td>
-			    <a href="index.php?p=deleteComment&amp;id=<?= $comments->getId() ?>"><input class="deleteComment" name="ButtonDeletePost" value="Supprimer" type="submit" /></a>
-			    <a href="index.php?p=editComment&amp;id=<?= $comments->getId() ?>"><input name="ButtonEditPost" value="Modifier" type="submit" /></a>
-            </td>
-        </tr>
+            <tr>
+                <th>Auteur</th>
+                <th>Commentaire</th>
+                <th>Date</th>
+                <th>Signalements</th>
+                <th>Suppression / Modification</th>
+            </tr>
 
-        <?php endforeach; ?>
+            <?php foreach ($allComments as $comments): ?>
 
-    </table>
+                <tr>
+                    <td><?= htmlspecialchars($comments->getAuthor());?></td>
+                    <td><?= htmlspecialchars(substr(strip_tags($comments->getComment()), 0, 60))?></td>
+                    <td><?= date('d/m/Y à H:i', strtotime($comments->getDate()))?></td>
+                    <td><?= $comments->getReport();?></td>
+                    <td>
+                        <div>
+                            <a href="index.php?p=deleteComment&amp;id=<?= $comments->getId() ?>"><input class="buttonAdminPost deleteComment" name="ButtonDeleteComment" value="Supprimer" type="submit" /></a>
+                        </div>
+                        <div>
+                            <a href="index.php?p=editComment&amp;id=<?= $comments->getId() ?>"><input class="buttonAdminPost editComment" name="ButtonEditPost" value="Modifier" type="submit" /></a>
+                        </div>
+                    </td>
+                </tr>
+
+            <?php endforeach; ?>
+
+        </table>
+
+    </section>
 
 <?php
 } else {
